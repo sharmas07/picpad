7e181bec7b4dc22b208434d9dd374b092529a255
sharmas07 <valis6435@gmail.com>
2024-02-10 13:18:56 +0530

    Revert "feat: UI changes"
    This reverts commit f5d9385372b1d5d08a44e34296b8d2f14b11492b.

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,27 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import baseURL from "../baseURL";
-import "../App.css";
 
-const Home = ({ text }) => {
+const Home = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [code, setCode] = useState("");
-  const [intervalId, setIntervalId] = useState(null);
-  const [displayText, setDisplayText] = useState(text);
   const navigate = useNavigate();
-
+  const [code, setCode] = useState();
   const handleCode = async (e) => {
-    if(code.length <=3){
-      return ;
-    }
+    
     setIsLoading(true);
     e.preventDefault();
+    console.log(code);
     try {
-      const { data } = await axios.post(`${baseURL}/checkuser`, {
-        username: code,
-      });
-      if (data) {
+      console.log()
+      const { data } = await axios.post(`${baseURL}/checkuser`, {username:code});
+      console.log(data);
+      if (data && code.length >=1) {
         localStorage.setItem("username", data.username);
         navigate("/gallery");
       }
@@ -30,69 +26,38 @@ const Home = ({ text }) => {
     }
     setIsLoading(false);
   };
-
-  const handleClick = () => { //beaware 
-    if (intervalId) {
-      clearInterval(intervalId);
-      setIntervalId(null);
-    } else {
-      const id = setInterval(generateText, 200);
-      setIntervalId(id);
-      setTimeout(() => {
-        clearInterval(id);
-      }, 5000); // Stop after 5 seconds
-    }
-  };
-
-  const generateText = () => {
-    let characters = "abcdefghijklmnopqrstuvwxyz1234567890~@!#$%^&*()?";
-    let length = Math.floor(Math.random() * 5000) + 50;
-    let result = "";
-    for (let i = 0; i < length; i++) {
-      result += characters.charAt(
-        Math.floor(Math.random() * characters.length)
-      );
-    }
-    setDisplayText((prevText) => prevText + result + " ");
-  };
-
   return (
-    <div className="main__container">
-      <div className="flex flex-col items-center justify-center min-h-[33rem] inner__container">
-        <h1 className="text-4xl text-center font-bold text-white mb-4 ">
-          Access Your Images From Anywhere
-        </h1>
-        <form
-          className="m-3 flex items-center justify-center "
-          onSubmit={handleCode}
-        >
-          <input
-            placeholder="Enter the code"
-            defaultValue=""
-            className="border border-gray-300 rounded-md py-2 px-4 leading-tight focus:outline-none focus:ring focus:ring-purple-500 mr-2"
-            type="text"
-            value={code}
-            onChange={(e) => {
-              const newCode = e.target.value;
-              if (newCode.length >= 1) {
+    <div className="flex flex-col items-center justify-center min-h-[33rem]">
+      <h1 className="text-4xl text-center font-bold text-white mb-4">
+        Access Your Images From Anywhere
+      </h1>
+      <form
+        className="m-3 flex items-center justify-center"
+        onSubmit={handleCode}
+      >
+        <input
+          placeholder="Enter the code"
+          defaultValue=""
+          className="border border-gray-300 rounded-md py-2 px-4 leading-tight focus:outline-none focus:ring focus:ring-purple-500 mr-2"
+          type="text"
+          value={code}
+          onChange={(e) => {
+            const newCode = e.target.value;
+            if (newCode.length >= 1) {
                 setCode(newCode);
-              }
-            }}
-          />
-          <button
-            onClick={(e) => {
-              handleCode(e);
-              handleClick();
-            }}
-            className="bg-purple-500 text-white py-2 px-4 rounded-md focus:outline-none focus:ring focus:ring-purple-500 buttons"
-          >
-            Open
-          </button>
-        </form>
-      </div>
+            }
+            
+          }}
+        />
+        <button
+          onClick={handleCode}
+          className="bg-purple-500 text-white py-2 px-4 rounded-md focus:outline-none focus:ring focus:ring-purple-500"
+          type="submit"
+        >
+          Open
+        </button>
+      </form>
       {isLoading && <h1>loading...</h1>}
-      <div className="text-container h-[34rem]">{displayText}</div>{" "}
-      {/* Use displayText */}
     </div>
   );
 };
